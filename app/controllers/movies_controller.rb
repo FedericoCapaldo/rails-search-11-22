809ce53c37adbc1efa8e8a934a31066aed81488a
Federico <federico.capaldo@hotmail.com>
2022-11-23 15:34:33 +0000

    added search bar and simple exact match query

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      @movies = Movie.where(title: params[:query])
+    else
+      @movies = Movie.all
+    end
   end
 end
