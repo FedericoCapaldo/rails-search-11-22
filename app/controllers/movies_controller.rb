4f1ef962375ec55295064d186b64383b1279ccb7
Federico <federico.capaldo@hotmail.com>
2022-11-23 16:44:05 +0000

    pg search: example of using pg search result in views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
   def index
     if params[:query].present?
-      @movies = Movie.search_by_title_and_synopsis(params[:query])
+      @results = PgSearch.multisearch(params[:query])
     else
       @movies = Movie.all
     end
