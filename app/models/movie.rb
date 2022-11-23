ab7f9a92f0c8ccb8594551cb08325e680d69e8da
Federico <federico.capaldo@hotmail.com>
2022-11-23 16:09:02 +0000

    pg search: added associated director table in search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,9 @@ class Movie < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_by_title_and_synopsis,
     against: [ :title, :synopsis ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
